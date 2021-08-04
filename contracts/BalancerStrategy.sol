472eaa7e9c23bc0096fbe6579c25c8885d82cdd1
OO <oo@OOs-MacBook-Pro.local>
2021-08-04 15:45:34 -0300

    add more aidrop functionality and tests

--- contracts/BalancerStrategy.sol
+++ contracts/BalancerStrategy.sol
@@ -15,6 +15,8 @@
 pragma solidity ^0.8.0;
 
 import "@mimic-fi/v1-core/contracts/interfaces/IStrategy.sol";
+import "@mimic-fi/v1-core/contracts/interfaces/ISwapConnector.sol";
+import "@mimic-fi/v1-core/contracts/interfaces/IVault.sol";
 import "@mimic-fi/v1-core/contracts/helpers/FixedPoint.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -30,7 +32,7 @@ contract BalancerStrategy is IStrategy {
     uint256 private constant JOIN_WEIGHTED_POOL_EXACT_TOKENS_IN_FOR_BPT_OUT = 1;
     uint256 private constant EXIT_WEIGHTED_POOL_EXACT_BPT_IN_FOR_ONE_TOKEN_OUT = 0;
 
-    address public immutable vault;
+    IVault public immutable vault;
     IERC20 public immutable token;
     IBalancerVault public immutable balancerVault;
     bytes32 public immutable poolId;
@@ -42,11 +44,11 @@ contract BalancerStrategy is IStrategy {
     string private _metadataURI;
 
     modifier onlyVault() {
-        require(vault == msg.sender, "CALLER_IS_NOT_VAULT");
+        require(address(vault) == msg.sender, "CALLER_IS_NOT_VAULT");
         _;
     }
 
-    constructor(address _vault, IERC20 _token, IBalancerVault _balancerVault, bytes32 _poolId, uint256 _tokenIndex, IERC20 _balToken, string memory _metadata) {
+    constructor(IVault _vault, IERC20 _token, IBalancerVault _balancerVault, bytes32 _poolId, uint256 _tokenIndex, IERC20 _balToken, string memory _metadata) {
         token = _token;
         balancerVault = _balancerVault;
         vault = _vault; 
@@ -58,7 +60,7 @@ contract BalancerStrategy is IStrategy {
 
         (poolAddress,) = _balancerVault.getPool(_poolId);
 
-        _token.approve(_vault, FixedPoint.MAX_UINT256);
+        _token.approve(address(_vault), FixedPoint.MAX_UINT256);
         _token.approve(address(_balancerVault), FixedPoint.MAX_UINT256);
     }
 
@@ -87,39 +89,23 @@ contract BalancerStrategy is IStrategy {
     }
 
     function onJoin(uint256 amount, bytes memory) external override onlyVault returns (uint256) {
-        (IERC20[] memory tokens,,) = balancerVault.getPoolTokens(poolId);
-
+        uint256 initialTokenBalance = token.balanceOf(address(this));
         uint256 initialBPTAmount = IERC20(poolAddress).balanceOf(address(this));
 
-        uint256[] memory amountsIn = new uint256[](tokens.length);
-        amountsIn[tokenIndex] = amount;
-
-        //TODO: use oracle for BPT price?
-        uint256 minimumBPT = 0;
-
-        IBalancerVault.JoinPoolRequest memory request = IBalancerVault.JoinPoolRequest({
-            assets: tokens,
-            maxAmountsIn: amountsIn,
-            userData: abi.encode(JOIN_WEIGHTED_POOL_EXACT_TOKENS_IN_FOR_BPT_OUT, amountsIn, minimumBPT),
-            fromInternalBalance: false
-        });
-
-        balancerVault.joinPool(
-            poolId,
-            address(this),
-            address(this),
-            request
-        );
+        claimAndInvest();
 
         uint256 finalBPTAmount = IERC20(poolAddress).balanceOf(address(this));
+        uint256 callerBPTAmount = amount.mul(finalBPTAmount.sub(initialBPTAmount)).div(initialTokenBalance);
 
         uint256 rate = _totalShares == 0? FixedPoint.ONE: _totalShares.div(finalBPTAmount);
-        uint256 shares = finalBPTAmount.sub(initialBPTAmount).mul(rate);
+        uint256 shares = callerBPTAmount.mul(rate);
         _totalShares = _totalShares.add(shares);
         return shares;
     }
 
     function onExit(uint256 shares, bytes memory) external override onlyVault returns (address, uint256) {
+        claimAndInvest();
+
         (IERC20[] memory tokens, , ) = balancerVault.getPoolTokens(poolId);
 
         uint256 initialTokenAmount = token.balanceOf(address(this));
@@ -155,19 +141,59 @@ contract BalancerStrategy is IStrategy {
         require(address(_token) != address(poolAddress), "BALANCER_INTERNAL_TOKEN");
         require(address(_token) != address(balToken), "BALANCER_INTERNAL_TOKEN");
 
-        _token.approve(vault, FixedPoint.MAX_UINT256);
+        _token.approve(address(vault), FixedPoint.MAX_UINT256);
     }
 
-    function investAll() external {
-        //TODO: repeated code from join
-
+    function investAllDAI() public {
         uint256 tokenBalance = token.balanceOf(address(this));
+        if(tokenBalance > 0) {
+            _investDAI(tokenBalance);
+        }
+    }
 
-        //JOIN pool with all DAI in balance
+    function tradeForDAI(IERC20 _tokenIn) public {
+        require(address(_tokenIn) != address(poolAddress), "BALANCER_INTERNAL_TOKEN");
+        require(address(_tokenIn) != address(token), "BALANCER_INTERNAL_TOKEN");
+        //TODO any other?
+
+        uint256 tokenInBalance = _tokenIn.balanceOf(address(this));
+
+        if(tokenInBalance > 0) {
+            uint256 deadline = block.timestamp + 5 minutes; //TODO, also optional
+
+            address swapConnector = vault.swapConnector();
+            
+            _tokenIn.transfer(swapConnector, tokenInBalance);
+
+            ISwapConnector(swapConnector).swap(
+                address(_tokenIn),
+                address(token),
+                tokenInBalance,
+                0, //TODO: should be check by connector using oracle
+                deadline,
+                ""
+            );
+        }
+    }
+
+    function tradeAndInvest(IERC20 _token) public {
+        tradeForDAI(_token);
+        investAllDAI();
+    }
+
+    function claimAndInvest() public {
+        //TODO: claim BAL
+        tradeAndInvest(balToken);
+    }
+
+
+    //Internal
+
+    function _investDAI(uint256 amount) internal {
         (IERC20[] memory tokens,,) = balancerVault.getPoolTokens(poolId);
 
         uint256[] memory amountsIn = new uint256[](tokens.length);
-        amountsIn[tokenIndex] = tokenBalance;
+        amountsIn[tokenIndex] = amount;
 
         //TODO: use oracle for BPT price?
         uint256 minimumBPT = 0;
@@ -185,13 +211,5 @@ contract BalancerStrategy is IStrategy {
             address(this),
             request
         );
-    }
-
-    function tradeForDAI(IERC20 _token) external {
-        require(address(_token) != address(poolAddress), "BALANCER_INTERNAL_TOKEN");
-        require(address(_token) != address(token), "BALANCER_INTERNAL_TOKEN");
-        //TODO any other?
-
-        //swap connector (slipage protection)
     }
 }
--- test/strategy.fork join.ts
+++ test/strategy.fork join.ts
@@ -4,7 +4,7 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-wit
 import { deploy, fp, bn, getSigner, impersonateWhale, instanceAt } from '@mimic-fi/v1-helpers'
 
 describe('BalancerStrategy - Join', function () {
-  let owner: SignerWithAddress, whale: SignerWithAddress, trader: SignerWithAddress, vault: Contract, strategy: Contract, dai: Contract, bal: Contract, bVault: Contract, bpt: Contract, weth: Contract
+  let whale: SignerWithAddress, trader: SignerWithAddress, vault: Contract, strategy: Contract, dai: Contract, bal: Contract, bVault: Contract, bpt: Contract, weth: Contract, usdc: Contract
 
   const BALANCER_VAULT = '0xBA12222222228d8Ba445958a75a0704d566BF2C8'
   const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
@@ -13,6 +13,8 @@ describe('BalancerStrategy - Join', function () {
   const TOKEN_INDEX = 0
   const BAL = '0xba100000625a3754423978a60c9317c58a424e3D'
   const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+  const UNISWAP_V2_ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
   const MAX_UINT_256 = bn(2).pow(256).sub(1)
 
@@ -39,16 +41,17 @@ describe('BalancerStrategy - Join', function () {
   }
 
   before('load signers', async () => {
-    owner = await getSigner()
     trader = await getSigner(1)
     whale = await impersonateWhale(fp(100))
   })
 
   before('deploy vault', async () => {
     const protocolFee = fp(0.00003)
-    const swapConnector = owner.address // random address
     const whitelistedStrategies: string[] = []
-    vault = await deploy('Vault', [protocolFee, swapConnector, whitelistedStrategies])
+
+    const swapConnector = await deploy('UniswapConnector', [UNISWAP_V2_ROUTER_ADDRESS])
+
+    vault = await deploy('Vault', [protocolFee, swapConnector.address, whitelistedStrategies])
   })
 
   before('load tokens', async () => {
@@ -57,6 +60,7 @@ describe('BalancerStrategy - Join', function () {
     bpt = await instanceAt('IERC20', POOL_ADDRESS)
     bal = await instanceAt('IERC20', BAL)
     weth = await instanceAt('IERC20', WETH)
+    usdc = await instanceAt('IERC20', USDC)
   })
 
   before('deposit to Vault', async () => {
@@ -175,38 +179,94 @@ describe('BalancerStrategy - Join', function () {
   })
 
   it('handle DAI airdrops', async () => {
-    await vault.connect(whale).join(whale.address, strategy.address, fp(50), '0x')
-
-    //airdrop 1
+    //airdrop 1000
     dai.connect(whale).transfer(strategy.address, fp(1000))
 
     //total shares = bpt
-    let bptBalance = await bpt.balanceOf(strategy.address)
-    let totalShares = await strategy.getTotalShares()
-    expect(totalShares).to.be.equal(bptBalance)
+    const initialBptBalance = await bpt.balanceOf(strategy.address)
+    const initialShares = await strategy.getTotalShares()
 
-    await vault.connect(whale).exit(whale.address, strategy.address, fp(0.5), '0x')
-
-    //dai not affected
-    let currentStrategyBalance = await dai.balanceOf(strategy.address)
-    expect(currentStrategyBalance).to.be.equal(fp(1000))
-
-    await vault.connect(whale).exit(whale.address, strategy.address, fp(1), '0x')
+    expect(initialShares).to.be.equal(initialBptBalance)
 
     //invest aidrop
-    await strategy.investAll()
+    await strategy.investAllDAI()
 
     //total shares < bpt
-    bptBalance = await bpt.balanceOf(strategy.address)
-    totalShares = await strategy.getTotalShares()
-    expect(totalShares.lt(bptBalance)).to.be.true
+    const finalBptBalance = await bpt.balanceOf(strategy.address)
+    const finalShares = await strategy.getTotalShares()
 
-    //dai sent to whale
-    currentStrategyBalance = await dai.balanceOf(strategy.address)
-    expect(currentStrategyBalance).to.be.equal(0)
-
-    //total shares = 0
-    totalShares = await strategy.getTotalShares()
-    expect(totalShares).to.be.equal(0)
+    expect(initialBptBalance.lt(finalBptBalance)).to.be.true
+    expect(initialShares).to.be.equal(finalShares)
   })
+
+  it('handle USDC airdrops', async () => {
+    //airdrop 1000
+    usdc.connect(whale).transfer(strategy.address, fp(1000).div(bn('1e12')))
+
+    const daiBalance = await dai.balanceOf(strategy.address)
+    expect(daiBalance).to.be.equal(0)
+
+    const initialBptBalance = await bpt.balanceOf(strategy.address)
+    const initialShares = await strategy.getTotalShares()
+
+    //invest aidrop
+    await strategy.tradeAndInvest(usdc.address)
+
+    const finalBptBalance = await bpt.balanceOf(strategy.address)
+    const finalShares = await strategy.getTotalShares()
+
+    expect(initialBptBalance.lt(finalBptBalance)).to.be.true
+    expect(initialShares).to.be.equal(finalShares)
+  })
+
+  it('handle USDC airdrops + Join', async () => {
+    //Make it so there are some previous shares
+    await vault.connect(whale).join(whale.address, strategy.address, fp(50), '0x')
+
+    const aidrop = fp(100000)
+    const joinAmount = fp(50)
+
+    //airdrop 1000
+    usdc.connect(whale).transfer(strategy.address, aidrop.div(bn('1e12')))
+
+    const daiBalance = await dai.balanceOf(strategy.address)
+    expect(daiBalance).to.be.equal(0)
+
+    const initialShares = await strategy.getTotalShares()
+
+    //whale joins
+    await vault.connect(whale).join(whale.address, strategy.address, joinAmount, '0x')
+
+    //Final token balance includes 100k airdrop + joinAmount
+    const finalTokenBalance = await strategy.getTokenBalance()
+    const finalShares = await strategy.getTotalShares()
+
+    const whaleSharesExpected = joinAmount.mul(initialShares).div(finalTokenBalance)
+    const whaleSharesObtained = finalShares.sub(initialShares)
+
+    //shares obtained by the whale should be close to how much dai it addd and not the airdropped one
+    expect(whaleSharesExpected.sub(whaleSharesObtained).abs().lt(fp(0.0001))).to.be.true
+  })
+
+  //TODO: need whale with BAL
+
+  // it('handle BAL airdrops', async () => {
+  //   //airdrop 1000
+  //   bal.connect(whale).transfer(strategy.address, fp(10))
+
+  //   const daiBalance = await dai.balanceOf(strategy.address)
+  //   expect(daiBalance).to.be.equal(0)
+
+  //   const initialBptBalance = await bpt.balanceOf(strategy.address)
+  //   const initialShares = await strategy.getTotalShares()
+
+  //   //invest aidrop
+  //   await strategy.claimAndInvest()
+
+  //   const finalBptBalance = await bpt.balanceOf(strategy.address)
+  //   const finalShares = await strategy.getTotalShares()
+
+  //   expect(initialBptBalance.lt(finalBptBalance)).to.be.true
+  //   expect(initialShares).to.be.equal(finalShares)
+  // })
 })
