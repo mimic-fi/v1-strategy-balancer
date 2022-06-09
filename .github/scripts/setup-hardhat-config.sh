83853ec62ca4fc2aa07eea00eb5c9991a12c326a
lkndjrt <lkndjrt@gmail.com>
2022-06-09 17:30:37 -0300

    chore: drop config template

--- .github/scripts/setup-hardhat-config.sh
+++ .github/scripts/setup-hardhat-config.sh
@@ -2,9 +2,17 @@
 MAINNET_URL="$1"
 POLYGON_URL="$2"
 
-cp ./scripts/networks.template.json ./scripts/networks.mimic.json
+set -o errexit
 
-sed -i -e "s/{{mainnet}}/${MAINNET_URL}/g" ./scripts/networks.mimic.json
-sed -i -e "s/{{polygon}}/${POLYGON_URL}/g" ./scripts/networks.mimic.json
-
-cp ./scripts/networks.mimic.json $HOME/.hardhat/networks.mimic.json
+echo "
+{
+  \"networks\": {
+    \"mainnet\": {
+      \"url\": \"${MAINNET_URL}\"
+    },
+    \"polygon\": {
+      \"url\": \"${POLYGON_URL}\"
+    }
+  }
+}
+" > $HOME/.hardhat/networks.mimic.json
