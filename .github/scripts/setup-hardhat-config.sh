fd92d4b36fc530b4676608598d8b2b51ef453b21
lkndjrt <lkndjrt@gmail.com>
2022-06-09 17:32:27 -0300

    chore: create hardhat dir if necessary

--- .github/scripts/setup-hardhat-config.sh
+++ .github/scripts/setup-hardhat-config.sh
@@ -4,15 +4,13 @@ POLYGON_URL="$2"
 
 set -o errexit
 
+mkdir -p $HOME/.hardhat
+
 echo "
 {
   \"networks\": {
-    \"mainnet\": {
-      \"url\": \"${MAINNET_URL}\"
-    },
-    \"polygon\": {
-      \"url\": \"${POLYGON_URL}\"
-    }
+    \"mainnet\": { \"url\": \"${MAINNET_URL}\" },
+    \"polygon\": { \"url\": \"${POLYGON_URL}\" }
   }
 }
 " > $HOME/.hardhat/networks.mimic.json
